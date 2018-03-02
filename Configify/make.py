49fc5d9c838361e62489d4cb17be406988f471e3
Dan Black <dyspop@gmail.com>
2018-03-02 16:17:45 -0500

    implement getpass and obscure

--- Configify/make.py
+++ Configify/make.py
@@ -1,26 +1,44 @@
 """Actually makes the configuration file."""
 
 import json
+import getpass
+import sys
 
 
-def __generate_config_dict(template):
+def __obscure(string):
+    """Obscure the data for display."""
+    deplorables = ["[", "]", "'", ",", " "]
+    obscured = str(['*' for char in string])
+    for deplorable in deplorables:
+        obscured = obscured.replace(deplorable, '')
+    return obscured
+
+
+def __generate_config_dict(template, secret, filename):
     """Generate a json config at the system path based on the template dict."""
     new_config = {}
     for k, v in template.items():
-        new_config[k] = input(
-            "'{k}' (default '{v}'   ): ".format(k=k, v=v)
-        ) or v
-        if new_config[k] == v:
-            print("Added '{k}': {v} to {t}".format(
-                k=k, v=v, t=str('').replace('_template', '')))
-    return new_config
+        if secret is True:
+            func = getpass.getpass
+        else:
+            func = input
+        new_config[k] = func("'{k}': ".format(k=k)) or v
+        if secret is True:
+            v = __obscure(new_config[k])
+        else:
+            v = new_config[k]
+        print("Set {k} to {v} in {f}".format(k=k, v=v, f=filename))
+        return new_config
 
 
-def make(filename='config.json', path='', data={}):
+def make(data={}, filename='config.json', path='', secret=True, get=False):
     """Make a file at the system path specified, or where run from."""
-    __generate_config_dict(template=data)
+    data = __generate_config_dict(template=data, secret=secret, filename=filename)
     path_and_name = '{p}{f}'.format(p=path, f=filename)
     config = open(path_and_name, 'w')
     data = json.dumps(data)
     config.write(str(data))
-    return {path_and_name: data}
+    if get is True:
+        return {path_and_name: data}
+    else:
+        pass
