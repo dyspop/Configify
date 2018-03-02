0bf62905eccadf1810c17372e3dfbcfe2f5e47be
Dan Black <dyspop@gmail.com>
2018-03-02 14:26:47 -0500

    add private generator

--- Configify/make.py
+++ Configify/make.py
@@ -3,8 +3,22 @@
 import json
 
 
-def make(filename='config.json', path='', data={'key': 'value'}):
+def __generate_json_from_template(template):
+    """Generate a json config at the system path based on the template dict."""
+    new_config = {}
+    for k, v in template.items():
+        new_config[k] = input(
+            "'{k}' (default '{v}'   ): ".format(k=k, v=v)
+        ) or v
+        if new_config[k] == v:
+            print("Added '{k}': {v} to {t}".format(
+                k=k, v=v, t=str('').replace('_template', '')))
+    return new_config
+
+
+def make(filename='config.json', path='', data={}):
     """Make a file at the system path specified, or where run from."""
+    __generate_json_from_template(template=data)
     path_and_name = '{p}{f}'.format(p=path, f=filename)
     config = open(path_and_name, 'w')
     data = json.dumps(data)
