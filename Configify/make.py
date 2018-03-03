211e6a9216a629d4ee58ceb6dcdf57904fba322d
Daniel Black <dyspop@gmail.com>
2018-03-03 01:53:33 -0500

    comments

--- Configify/make.py
+++ Configify/make.py
@@ -27,27 +27,45 @@ def __obscure(string):
 
 def __generate_config_dict(template, secret, filename):
     """Generate a json config at the system path based on the template dict."""
-    new_config = {}
-    for k, v in template.items():
-        if secret is True:
-            func = getpass.getpass
-            if v != ('' or None or False):
-                prompt = "Enter value for \"{k}\"\n(return for default \"{v}\")': ".format(
-                    k=k, v=__obscure(v))
+    # Don't do stuff unless we got a dict
+    if isinstance(template, dict):
+        new_config = {}
+        # loop through key-value pairs
+        for k, v in template.items():
+            # check if we're in the secret state
+            if secret is True:
+                # set the function to call user input
+                func = getpass.getpass
+                # check that there's a default
+                if v != ('' or None or False):
+                    # if there is then prompt with the default but obscured
+                    prompt = "Enter value for \"{k}\"\n(return for default \"{v}\")': ".format(
+                        k=k, v=__obscure(v))
+                else:
+                    # but if there isn't one then obviously don't try to display it
+                    prompt = "'{k}: ".format(k=k)
+            # if we're not handling secrets
             else:
-                prompt = "'{k}: ".format(k=k)
-        else:
-            func = input
-            prompt = "Enter value for \"{k}\"\n(return for default \"{v}\")': ".format(
-                k=k, v=v)
-        print(prompt)
-        new_config[k] = func() or v
-        if secret is True:
-            v_display = __obscure(new_config[k])
-        else:
-            v_display = new_config[k]
-        print("Set \"{k}\" to \"{v}\" in {f}".format(k=k, v=v_display, f=filename))
-        return new_config
+                # just use normal input
+                func = input
+                # and don't obscure the default values
+                prompt = "Enter value for \"{k}\"\n(return for default \"{v}\")': ".format(
+                    k=k, v=v)
+            # print the prompt
+            print(prompt)
+            # set the value of the user input, if not to the default
+            new_config[k] = func() or v
+            # check secretness again, if so set the new value's display version to be obscured
+            if secret is True:
+                v_display = __obscure(new_config[k])
+            # if not don't obscure it
+            else:
+                v_display = new_config[k]
+            # print the results
+            print("Set \"{k}\" to \"{v}\" in {f}".format(k=k, v=v_display, f=filename))
+            return new_config
+    else:
+        return None
 
 
 def make(data={}, filename='config.json', path='', secret=True, get=False):
