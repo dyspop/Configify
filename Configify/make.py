db42df79271c595e3debf6c25b916339b96979bf
Daniel Black <dyspop@gmail.com>
2018-03-03 02:58:05 -0500

    unflake

--- Configify/make.py
+++ Configify/make.py
@@ -1,14 +1,13 @@
 """Actually makes the configuration file.
 
 Configify  Copyright (C) 2018  Dan Black
-    This program comes with ABSOLUTELY NO WARRANTY; 
+    This program comes with ABSOLUTELY NO WARRANTY;
     This is free software, and you are welcome to redistribute it
     under certain conditions; For details see license.txt
 
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
-
 
 
 import json
@@ -20,6 +19,10 @@ def __obscure(string, char='*'):
     """Obscure the data for display."""
     return char * len(string)
 
+prompt_text = """Enter value for \"{k}\"
+(return for default \"{v}\")': """
+
+set_confirmation_text = "Set \"{k}\" to \"{v}\" in {f}"
 
 def __generate_config_dict(template, secret, filename, char):
     """Generate a json config at the system path based on the template dict."""
@@ -30,28 +33,25 @@ def __generate_config_dict(template, secret, filename, char):
         for k, v in template.items():
             # check if we're in the secret state
             if secret is True:
-                # set the function to call user input
-                func = getpass.getpass
                 # check that there's a default
                 if v != ('' or None or False):
                     # if there is then prompt with the default but obscured
-                    prompt = "Enter value for \"{k}\"\n(return for default \"{v}\")': ".format(
-                        k=k, v=__obscure(v, char))
+                    prompt = prompt_text.format(k=k, v=__obscure(v, char))
                 else:
-                    # but if there isn't one then obviously don't try to display it
+                    # but if there isn't one then don't try to display it
                     prompt = "'{k}: ".format(k=k)
+                # set the value of the user input, if not to the default
+                new_config[k] = getpass.getpass(prompt)
             # if we're not handling secrets
             else:
-                # just use normal input
-                func = input
-                # and don't obscure the default values
-                prompt = "Enter value for \"{k}\"\n(return for default \"{v}\")': ".format(
-                    k=k, v=v)
+                # don't obscure the default values if not secret
+                prompt = prompt_text.format(k=k, v=v)
+                new_config[k] = input(prompt)
             # print the prompt
             print(prompt)
             # set the value of the user input, if not to the default
-            new_config[k] = func() or v
-            # check secretness again, if so set the new value's display version to be obscured
+            # new_config[k] = func() or v
+            # if secret set the new value's display version to be obscured
             if secret is True:
                 v_display = __obscure(new_config[k], char)
             # if not don't obscure it
@@ -60,7 +60,7 @@ def __generate_config_dict(template, secret, filename, char):
             # print the results, but delete the extra line from the user if they entered the default
             if v == new_config[k]:
                 print('\033[F')
-            print("Set \"{k}\" to \"{v}\" in {f}".format(k=k, v=v_display, f=filename))
+            print(set_confirmation_text.format(k=k, v=v_display, f=filename))
             return new_config
     else:
         return None
