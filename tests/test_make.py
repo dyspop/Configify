7e95d2c692eb06f872565dd16daf13377dc1c4f6
Daniel Black <dyspop@gmail.com>
2018-03-03 02:39:48 -0500

    comments

--- tests/test_make.py
+++ tests/test_make.py
@@ -7,6 +7,7 @@ path = 'tests/'
 filename = 'config.json'
 
 def test_make_makes_json_at_path():
+    """Did the json file get made at the path we said."""
     Configify.make(path=path)
     config_file = '{p}{f}'.format(p=path, f=filename)
     assert open(config_file, 'r')
@@ -14,6 +15,7 @@ def test_make_makes_json_at_path():
 
 
 def test_switching_inputs_returns_error():
+    """If we switch params does it fail with the right error."""
     Configify.make(path=path)
     config_file = '{f}{p}'.format(p=path, f=filename)
     with pytest.raises(FileNotFoundError):
@@ -21,6 +23,7 @@ def test_switching_inputs_returns_error():
 
 
 def test_make_makes_json_file_by_eof():
+    """Did the json file we wrote end in .json."""
     assert list(
         Configify.make(path=path, get=True).keys()
     )[0].endswith('.json')
@@ -28,6 +31,7 @@ def test_make_makes_json_file_by_eof():
 
 
 def test_make_makes_json_actually():
+    """Did the file we wrote actually become json."""
     Configify.make(path=path, data={"foo": "bar"})
     config_file = '{p}{f}'.format(p=path, f=filename)
     assert json.load(open(config_file))
